d293908b0ccfa1990012855059f0512224a8b918
thongdeptraiwa <thong442001@gmail.com>
2025-01-05 11:17:51 +0700

    5/1-3

--- ios/AnleneTests/AnleneTests.m
+++ ios/AnleneTests/AnleneTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface AnleanTests : XCTestCase
+@interface AnleneTests : XCTestCase
 
 @end
 
-@implementation AnleanTests
+@implementation AnleneTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
